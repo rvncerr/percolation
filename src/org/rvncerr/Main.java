40a147d5fed29c22360de95df6ddcf94140cf7e1
Dmitriy Kalugin-Balashov <rvncerr@rvncerr.org>
2015-05-18 19:26:09 +0300

    Simplify.

--- src/org/rvncerr/Main.java
+++ src/org/rvncerr/Main.java
@@ -3,45 +3,38 @@ package org.rvncerr;
 // Процеживание в многомерных кубах.
 
 public class Main {
-    public static final int DIM_MIN = 1;
-    public static final int DIM_MAX = 10;
-    public static final int EDGE_MIN = 1;
-    public static final int EDGE_MAX = 100;
-    public static final int ITER_COUNT = 10;
-    public static final int CELL_LIMIT = 1000000;
     public static void main(String[] args) {
+        int dim = Integer.parseInt(args[0]);
+        int edgeSize = Integer.parseInt(args[1]);
+        int iterCount = Integer.parseInt(args[2]);
+        int cellCount = (int)Math.pow(edgeSize, dim);
+        int neighbourCount = (int)Math.pow(2, dim);
 
-        System.out.println("dim;neig;edge;cell;mean;var;");
-        for(int d = DIM_MIN; d <= DIM_MAX; d++) {
-            for(int e = EDGE_MIN; e <= EDGE_MAX; e++) {
-                int c = (int)Math.pow(e, d);
-                int n = (int)Math.pow(2, d);
-                if(c > CELL_LIMIT) continue;
+        double[] results = new double[iterCount];
+        double mean = 0;
+        double var = 0;
 
-                double[] x = new double[ITER_COUNT];
-                double mean = 0;
-                for (int i = 0; i < ITER_COUNT; i++) {
-                    Percolation p = new Percolation(d, e);
-                    while (!p.percolates()) p.open();
-                    x[i] = 1. * p.statOpened / p.cellsTotal;
-                    mean += x[i];
-                }
-                mean /= ITER_COUNT;
-                double var = 0;
-                for (int i = 0; i < ITER_COUNT; i++) {
-                    var += Math.pow(x[i] - mean, 2);
-                }
-                var /= (ITER_COUNT - 1);
-
-                StringBuilder stringBuilder = new StringBuilder();
-                stringBuilder.append(d).append(';').append(n)
-                        .append(';').append(e).append(';')
-                        .append(c).append(';').append(mean)
-                        .append(';').append(var).append(';');
-                System.out.println(stringBuilder);
-            }
-
-
+        for (int i = 0; i < iterCount; i++) {
+            Percolation p = new Percolation(dim, edgeSize);
+            while (!p.percolates()) p.open();
+            results[i] = 1. * p.statOpened / p.cellsTotal;
+            mean += results[i];
         }
+        mean /= iterCount;
+
+        for (int i = 0; i < iterCount; i++) {
+            var += Math.pow(results[i] - mean, 2);
+        }
+        var /= (iterCount - 1);
+
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("dimension = ").append(dim)
+                .append("\nedge_size = ").append(edgeSize)
+                .append("\niter_count = ").append(iterCount)
+                .append("\ncell_count = ").append(cellCount)
+                .append("\nneighbour_count = ").append(neighbourCount)
+                .append("\n\nmean = ").append(mean)
+                .append("\nvar = ").append(var);
+        System.out.println(stringBuilder);
     }
 }
