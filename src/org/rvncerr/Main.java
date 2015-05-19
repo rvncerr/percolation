a331f1ca9d8b5e76a4edc7ffd3bacb22a8b5e9c8
Dmitriy Kalugin-Balashov <rvncerr@rvncerr.org>
2015-05-19 20:20:05 +0300

    Usage.

--- src/org/rvncerr/Main.java
+++ src/org/rvncerr/Main.java
@@ -2,8 +2,24 @@ package org.rvncerr;
 
 // Процеживание в многомерных кубах.
 
+/*
+MacBook-Air-dkalugin-balashov:Perlocation rvncerr$ java -classpath ./out/production/Perlocation/ org.rvncerr.Main 2 100 100
+dimension = 2
+edge_size = 100
+iter_count = 100
+cell_count = 10000
+neighbour_count = 4
+
+mean = 0.5943240000000001
+var = 2.6403982222222226E-4
+*/
+
 public class Main {
     public static void main(String[] args) {
+        if(args.length != 3) {
+            System.out.println("usage: java -classpath <classpath> org.rvncerr.Main <dim> <edge> <iter_count>");
+            return;
+        }
         int dim = Integer.parseInt(args[0]);
         int edgeSize = Integer.parseInt(args[1]);
         int iterCount = Integer.parseInt(args[2]);
